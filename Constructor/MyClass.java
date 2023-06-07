f158825178ab53ab9bd695f9a05b87a7fde5951c
Mahin5580 <123983742+Mahin5580@users.noreply.github.com>
2023-06-08 02:55:55 +0600

    Add files via upload

---
+++ Constructor/MyClass.java
@@ -0,0 +1,15 @@
+package Constructor;
+
+public class MyClass {
+
+	int x;
+	public void jd() {
+		x=5;
+		System.out.println(x);
+	}
+	public static void main(String[] args) {
+		MyClass n1=new MyClass();
+		n1.jd();
+		
+	}
+}
